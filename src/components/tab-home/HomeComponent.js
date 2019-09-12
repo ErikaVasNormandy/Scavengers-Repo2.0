999c62d9265e08b20e8d166bc9ac7519578b2c9c
Erika Hudiono <erikavasnormandy@gmail.com>
2019-09-11 22:25:31 -0700

    reversed

--- src/components/tab-home/HomeComponent.js
+++ src/components/tab-home/HomeComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import TileComponent from '../BaseTile/TileComponent';
 
+import moment from 'moment';
 
 import {styles} from './home.css';
 import {overallstyles} from '../../App.css';
@@ -15,9 +16,9 @@ class HomeComponent extends React.Component{
 		super(props)
 		this.state={
 			field: "value",
-			contents: "",
+			contents: [],
 			htmlcontents:"",
-			testcontents:"",
+			testcontents:[],
 			listOfNamesToQuery:[],
 			rawActualContents:[],
 			queriedHTML: [],
@@ -58,7 +59,6 @@ class HomeComponent extends React.Component{
 		
 		.catch(err => console.log(err))
 		*/
-		
 		/// Get first a list of all the *.html files in that directory
 ///	axios.get('https://api.github.com/repos/erikavasnormandy/ErikaVasNormandy.github.io/contents/HomePosts', { headers: {Authorization: `Bearer  `}  }
 		axios.get('https://api.github.com/repos/erikavasnormandy/ErikaVasNormandy.github.io/contents/HomePosts', { headers: {Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`}  }
@@ -73,15 +73,23 @@ class HomeComponent extends React.Component{
 //					console.log(this.state.contents.length)
 					
 					var placeholder = this.state.contents.length
+
+					var newSortedArray = this.state.contents.sort((a,b)=>   a.name > b.name)
+
+		//			window.alert(JSON.stringify(newSortedArray[0]))
+//					window.alert(newSortedArray[0].name)
 					//console.log("Hello from get contents")
 					for(var i=0;i<placeholder; i++){
 						//console.log("contents are: ", this.state.contents[i])
 						var stringQuery = "https://api.github.com/repos/erikavasnormandy/ErikaVasNormandy.github.io/contents/HomePosts/" + this.state.contents[i].name
 						//console.log(stringQuery)
+
 						axios.get(stringQuery, { headers: {Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`}  })
 						.then(res=>{
 							if(res.data){
+
 								this.setState({queriedHTML: this.state.queriedHTML.concat(res.data),
+
 								})
 								//this.setState({ queriedHTML: this.state.queriedHTML.concat(atob(res.data.content))})
 								//console.log("calling from the getContents inner loop ", this.state.queriedHTML)
@@ -95,6 +103,12 @@ class HomeComponent extends React.Component{
 	}
 
 	
+
+
+	getHTMLContents(){
+
+	}
+
 	render()
 	{
 		let {queriedHTML} = this.state;
@@ -107,17 +121,37 @@ class HomeComponent extends React.Component{
 				<h1>Home</h1>
 					{this.state.hasLoaded ? <div className="loader"></div>: null}
 
-				<ul >
+					{/*{window.alert(moment(moment.now()).format('MM/DD/YYYY'))}*/}
 
-				{this.state.queriedHTML.map(item => (
-						
-						<li key={item} > 		
 
+					{/*window.alert(moment(moment.now()).format('MM-DD-YYYY'))}}*/}
+
+
+				<ul>
+	{	/*		{this.state.queriedHTML.sort((a,b) => a.name > b.name).map(item => (*/}
+
+		{/* {this.state.queriedHTML.sort((a,b) => {
+
+				return moment(a.name).isAfter(moment(b.name)) ? -1: 1;
+			})
+*/}
+
+{/*			{this.state.queriedHTML.sort((a,b) => a.name > b.name).map(item => (
+*/}
+			{this.state.queriedHTML.sort((a,b) =>a.name.localeCompare(b.name)).reverse().map(item => (
+
+						<li key={item} > 				
+							<p>{item.name}</p>
 							<TileComponent bodyProp={atob(item.content)} buttonProp={"https://erikavasnormandy.github.io/HomePosts/".concat(item.name)}/>
 						</li>
-					))
+					)
+					)
 				}
 				</ul>
+
+
+
+
 			</div>		
 		);
 	}	
