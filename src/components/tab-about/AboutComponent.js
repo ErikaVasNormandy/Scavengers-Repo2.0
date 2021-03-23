9449633a1cabc464fe8de8a8c9c34d712c7a219e
Erika Hudiono <erikavasnormandy@gmail.com>
2021-03-22 17:59:39 -0700

    Update AboutComponent.js

--- src/components/tab-about/AboutComponent.js
+++ src/components/tab-about/AboutComponent.js
@@ -16,10 +16,10 @@ class AboutComponent extends React.Component{
 
       	<div className = " columnAbout2">
       	<h2>Where to Find:</h2>
-	    <p>ArtStation: <a href="https://www.artstation.com/technomancer-01">Technomancer-01</a></p>
-	    <p><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/Instagram_AppIcon_Aug2017.png" width="200" height="200"/> <a href="https://www.instagram.com/technomancer_one/">Technomancer_One</a></p>
-	    <p>Twitter: <a href="https://twitter.com/Technomancer_01">Technomancer_01</a></p>
-	    <p>RedBubble: <a href="https://redbubble.com/people/Technomancer-01/shop">Technomancer-01</a></p>
+	    <h2>ArtStation: <a href="https://www.artstation.com/technomancer-01"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/artstation.png" width="50" height="50" />Technomancer-01</a></h2>
+	    <h2> <a href="https://www.instagram.com/technomancer_one/"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/Instagram_AppIcon_Aug2017.png" width="50" height="50"/>Technomancer_One</a></h2>
+	    <h2>Twitter: <a href="https://twitter.com/Technomancer_01"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/twittericon.jpg" width="50" height="50" />Technomancer_01</a></h2>
+	    <h2>RedBubble: <a href="https://redbubble.com/people/Technomancer-01/shop"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/redbubble.png" width="50" height="50" />Technomancer-01</a></h2>
 	 </div>
       </div>
     );
