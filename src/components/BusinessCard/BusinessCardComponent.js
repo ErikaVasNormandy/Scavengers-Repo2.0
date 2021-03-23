74dcf156fe3e45261ac7a65cbc61564f721c5359
Erika Hudiono <erikavasnormandy@gmail.com>
2021-03-22 18:03:33 -0700

    Update BusinessCardComponent.js

--- src/components/BusinessCard/BusinessCardComponent.js
+++ src/components/BusinessCard/BusinessCardComponent.js
@@ -12,14 +12,14 @@ class BusinessCardComponent extends Component {
           alt="Huh couldn't find it"></img>
 
           
-          <div className = "sideMenuText">	<p>Where to Find Me</p>
+          <div className = "sideMenuText">	<h3>Where to Find Me</h3>
 
             <ul className="sideMenuLinks">
-                <li className="sideMenuLinksLI"> <a className="sideMenuLinksA" href="https://erikavasnormandy.artstation.com">Artstation</a></li>
-      	  	<li className="sideMenuLinksLI"> <a className="sideMenuLinksA" href="https://www.instagram.com/technomancer_one/">Instagram</a></li>
-                <li className="sideMenuLinksLI"><a className="sideMenuLinksA" href="https://github.com/ErikaVasNormandy">GitHub</a></li>
-                <li className="sideMenuLinksLI"><a className="sideMenuLinksA" href="https://twitter.com/Technomancer_01">Twitter</a></li>
-                <li className="sideMenuLinksLI"><a className="sideMenuLinksA" href="https://www.redbubble.com/people/Technomancer-01/shop?asc=u">RedBubble</a></li>		
+
+	  <h2><a href="https://www.artstation.com/technomancer-01"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/artstation.png" width="50" height="50" />Technomancer-01</a></h2>
+	    <h2> <a href="https://www.instagram.com/technomancer_one/"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/Instagram_AppIcon_Aug2017.png" width="50" height="50"/>Technomancer_One</a></h2>
+	    <h2><a href="https://twitter.com/Technomancer_01"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/twittericon.jpg" width="50" height="50" />Technomancer_01</a></h2>
+	    <h2><a href="https://redbubble.com/people/Technomancer-01/shop"><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/redbubble.png" width="50" height="50" />Technomancer-01</a></h2>
             </ul>
           </div>
 
