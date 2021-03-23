e2280f1bb9dc40f9f81aa7679c9178fd94c7bc30
Erika Hudiono <erikavasnormandy@gmail.com>
2021-03-22 17:41:22 -0700

    Update AboutComponent.js

--- src/components/tab-about/AboutComponent.js
+++ src/components/tab-about/AboutComponent.js
@@ -11,26 +11,16 @@ class AboutComponent extends React.Component{
       <div className ="row">
       	<div className = " columnAbout1">
       		<h2>About </h2>	
-          <img className="profilePicture" src="https://cdna.artstation.com/p/assets/images/images/025/716/346/large/erika-hudiono-asset.jpg?1586712390"/>
-      		<p>- Exploring stacks</p>
-          <p>- Gaming on casual mode</p>
-          <p>- Drawing and writing </p>
-          <p>- Tinkering in meatspace</p>
-
-
-          <p>If you ever want an ice breaker, just say you like rabbits, show me your rabbit pictures, or even bring one if you too are a fan of Leporidae.</p>
-          <p className="tags"> Games, Neil Gaiman Comics, Folding Chair Historian, and Rabbits.</p>
-
+		<img className="profilePicture" src={roundrobin} alt = "Huh image must have moved datacenters"/>
       	</div>
 
       	<div className = " columnAbout2">
-      	<h2>Contact</h2>
-      	  		<p>Github: <a href="https://github.com/ErikaVasNormandy">Erika Vas Normandy</a></p>
-      	  		<p>Instagram: <a href="https://www.instagram.com/s0meCasual/">S0meCasual</a></p>
-		<h2>ShopFronts</h2>
-      	  		<p>RedBubble: <a href="https://redbubble.com/people/Technomancer-01/shop">Technomancer-01</a></p>
-      	      	  	<p>ArtStation: <a href="https://erikavasnormandy.artstation.com">Actual Portfolio</a></p>
-			<img className="profilePicture" src={roundrobin} alt = "Huh image must have moved datacenters"/>
+      	<h2>Where to Find:</h2>
+	    <p>ArtStation: <a href="https://www.artstation.com/technomancer-01">Technomancer-01</a></p>
+	    <p>Instagram: <a href="https://www.instagram.com/technomancer_one/">Technomancer_One</a></p>
+	    <p>Github: <a href="https://github.com/ErikaVasNormandy">Erika Vas Normandy</a></p>
+	    <p>Twitter: <a href="https://twitter.com/Technomancer_01">Technomancer_01</a></p>
+	    <p>RedBubble: <a href="https://redbubble.com/people/Technomancer-01/shop">Technomancer-01</a></p>
 	 </div>
       </div>
     );
