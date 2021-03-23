30200d617e9dce6e830ed60fa6cac042eceb28c7
Erika Hudiono <erikavasnormandy@gmail.com>
2021-03-22 17:50:27 -0700

    Update AboutComponent.js

--- src/components/tab-about/AboutComponent.js
+++ src/components/tab-about/AboutComponent.js
@@ -17,8 +17,7 @@ class AboutComponent extends React.Component{
       	<div className = " columnAbout2">
       	<h2>Where to Find:</h2>
 	    <p>ArtStation: <a href="https://www.artstation.com/technomancer-01">Technomancer-01</a></p>
-	    <p>Instagram: <a href="https://www.instagram.com/technomancer_one/">Technomancer_One</a></p>
-	    <p>Github: <a href="https://github.com/ErikaVasNormandy">Erika Vas Normandy</a></p>
+	    <p><img src="https://s3-us-west-1.amazonaws.com/scavengers-repo.com/social+media+icons/Instagram_AppIcon_Aug2017.png"/>Instagram: <a href="https://www.instagram.com/technomancer_one/">Technomancer_One</a></p>
 	    <p>Twitter: <a href="https://twitter.com/Technomancer_01">Technomancer_01</a></p>
 	    <p>RedBubble: <a href="https://redbubble.com/people/Technomancer-01/shop">Technomancer-01</a></p>
 	 </div>
